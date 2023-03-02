0b4b93c68a59617956b9e669033f5a98e373e979
Nick Cao <nickcao@nichi.co>
2023-03-02 16:06:58 +0800

    add comment

--- submit.sh
+++ submit.sh
@@ -4,6 +4,12 @@
 #SBATCH --nodes=2
 #SBATCH --ntasks-per-node=128
 
+# Run POT3D with isc2023 input on both PSC bridges-2 and FAU Fritz CPU clusters using 4 nodes.
+# Experiment with number of ranks per socket/numa domains to get the best results.
+# Your job should converge at 25112 steps and print outputs like below:
+# ### The CG solver has converged.
+# Iteration:    25112   Residual:   9.972489313728662E-13
+
 module load openmpi/4.0.5-nvhpc22.9
 module load hdf5/1.10.7-gcc10.2.0
 module load python/3.8.6

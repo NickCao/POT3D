9837f73adbecc2b1eef3ff704c988d7d5f9f387e
Nick Cao <nickcao@nichi.co>
2023-03-02 10:59:18 +0800

    simplify validation.sh

--- scripts/pot3d_validation.sh
+++ scripts/pot3d_validation.sh
@@ -1,53 +1,14 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-new_run=$1
-valid_run=$2
+set -euo pipefail
 
-cX="\033[0m"
-cG="\033[32m"
-echo="echo -e"
+new=$(tail -n 4 "$1")
+ref=$(tail -n 4 "$2")
 
-br1=$(grep "Energy in Br" $1)
-br2=$(grep "Energy in Br" $2)
-
-bt1=$(grep "Energy in Bt" $1)
-bt2=$(grep "Energy in Bt" $2)
-
-bp1=$(grep "Energy in Bp" $1)
-bp2=$(grep "Energy in Bp" $2)
-
-if [ "$br1" == "" ]; then
-   ${echo} "Run may have ${cR}FAILED${cX} to run!"
-   exit 1
-fi
-
-passed=0
-
-if [ "$br1" != "$br2" ]; then
-  passed=$(($passed + 1))
-fi	
-
-if [ "$bt1" != "$bt2" ]; then
-  passed=$(($passed + 1))
-fi
-
-if [ "$bp1" != "$bp2" ]; then
-  passed=$(($passed + 1))
-fi
-
-if [ $passed == 0 ]; then
-  ${echo} "Run has ${cG}PASSED${cX} validation!"
+if [ "$new" == "$ref" ]; then
+  echo "PASSED"
+  exit 0
 else
-  ${echo} "Run may have ${cR}FAILED${cX} validation!"
-  echo " "
-  echo "RUN1 BR: $br1"
-  echo "RUN2 BR: $br1"
-  echo " "
-  echo "RUN1 BT: $bt1"
-  echo "RUN2 BT: $bt1"
-  echo " "
-  echo "RUN1 BP: $bp1"
-  echo "RUN2 BP: $bp1"
+  echo "FAILED"
+  exit 1
 fi
-
-exit 0

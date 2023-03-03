8bd86e9c99a610eea84bfefe89df88ce2c1f8b6b
Nick Cao <nickcao@nichi.co>
2023-03-03 10:23:05 +0800

    give up fiddling with intelmpi

--- submit.sh
+++ submit.sh
@@ -38,10 +38,9 @@ case "$TOOLCHAIN" in
   intel)
     module load openmpi/4.0.2-intel20.4
     module load hdf5/1.12.0-intel20.4
-    MPIARG=("--mca" "btl" '^openib')
+    MPIARG=()
     ;;
   fau)
-    module load gcc/12.2.0
     module load openmpi/4.1.4-gcc12.2.0
     module load hdf5/1.12.2-gcc12.2.0-ompi
     MPIARG=("--mca" "btl" '^openib')

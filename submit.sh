dbeea3e9fdeb0c5526e724915592205e591826a4
Nick Cao <nickcao@nichi.co>
2023-04-27 14:09:34 +0800

    add gpu support

--- submit.sh
+++ submit.sh
@@ -12,14 +12,26 @@ if [ "$CLUSTER" == "fau" ]; then
 fi
 
 case "$CLUSTER-$TOOLCHAIN-$COMM" in
+  bridges2-gnu-openmpi)
+    spack load gcc@12.2.0
+    spack load hdf5%gcc/7vsroem
+    spack load openmpi/hyo5ews
+    ;;
+  bridges2-intel-openmpi)
+    spack load intel-oneapi-compilers
+    spack load hdf5%intel
+    spack load openmpi/s7i7wcq
+    export CC=icc FC=ifort
+    ;;
   bridges2-intel-intelmpi)
     # spack install intel-oneapi-compilers
     # spack compiler add `spack location -i intel-oneapi-compilers`/compiler/latest/linux/bin/intel64
     # spack install openmpi%intel+legacylaunchers fabrics=ucx schedulers=slurm
     # spack install hdf5%intel+fortran+hl~mpi
     # patch with https://github.com/mesonbuild/meson/pull/10056
-    spack load intel-oneapi-compilers
-    spack load intel-oneapi-mpi
+    spack load intel-oneapi-compilers@2023.1.0
+    spack load intel-oneapi-mpi@2021.9.0/c5cjhoq
+    spack load intel-oneapi-vtune@2023.1.0
     spack load hdf5%intel
     export CC=icc FC=ifort IFORTCFG=/dev/null
     ;;
@@ -56,3 +68,4 @@ meson compile -C "$JOBDIR/builddir"
   --pot3d     "$JOBDIR/builddir/pot3d" \
   --workdir   "$JOBDIR/work" \
   --testsuite "$SOURCEDIR/testsuite/$TESTSUITE"
+#  --testsuite "$SOURCEDIR/testsuite/$TESTSUITE" vtune -collect hotspots -result-dir result
